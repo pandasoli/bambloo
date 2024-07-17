9c9725f7f1e7e2b49bf1151f99271d7adf1830fa
Panda Soli <pandasoli+git@vivaldi.net>
2024-07-17 02:06:44 -0300

    📋: Add sass for more comfortability

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './public/manifest.json'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import sveltePreprocess from 'svelte-preprocess'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-		svelte(),
+		svelte({
+			preprocess: sveltePreprocess()
+		}),
 		tsconfigPaths(),
 		crx({ manifest: manifest as ManifestV3Export })
 	]
