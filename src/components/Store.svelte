9e9e13aae24224c2ebbda70d93703ff0d6ab7f64
Eli Soli <elisoli+git@vivaldi.net>
2024-12-27 23:37:12 -0300

    🫧 block store search until it's not implemented

--- src/components/Store.svelte
+++ src/components/Store.svelte
@@ -144,7 +144,7 @@
 	{:else}
 		<div id='searchbox'>
 			<div>
-				<input type='text' placeholder='Search here...' />
+				<input type='text' disabled placeholder='Search here...' />
 
 				<div>
 					<svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
@@ -272,6 +272,8 @@
 		display: grid;
 		place-items: center;
 		height: 38px;
+
+		* { cursor: not-allowed !important }
 
 		& > div {
 			position: relative;
